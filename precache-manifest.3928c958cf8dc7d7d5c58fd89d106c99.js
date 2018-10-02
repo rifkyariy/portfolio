70f3f2e48e9da12a5e7fa55342780c8282c67581
kryif <rifkyariy@gmail.com>
2018-10-02 11:33:15 +0700

    Updates

---
+++ precache-manifest.3928c958cf8dc7d7d5c58fd89d106c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "2ef3b573a2e4952d515f",
+    "url": "/portfolio/static/js/main.2ef3b573.chunk.js"
+  },
+  {
+    "revision": "881d1b40e5ae5bfca406",
+    "url": "/portfolio/static/js/1.881d1b40.chunk.js"
+  },
+  {
+    "revision": "2ef3b573a2e4952d515f",
+    "url": "/portfolio/static/css/main.0ee90050.chunk.css"
+  },
+  {
+    "revision": "43e20919b8740df7add688dc8ced5964",
+    "url": "/portfolio/index.html"
+  }
+];
